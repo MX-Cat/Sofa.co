03e3a9f020e6e93c5949310215600d3ae411124f
Maxim Krouk <maximkrouk@gmail.com>
2018-12-24 00:54:19 +0300

    Initial commit

---
+++ Sofa.co/js/popup.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function callback() {
+    if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+    } else {
+
+    }
+}
+
+function ShowOptions() {
+    if (chrome.runtime.openOptionsPage) {
+        chrome.runtime.openOptionsPage(callback());
+    } else {
+        window.open(chrome.runtime.getURL('options.html'));
+    }
+}
+
+
+
+window.onload = function () {
+    document.querySelector('#go-to-options').addEventListener('click', ShowOptions);
+}
