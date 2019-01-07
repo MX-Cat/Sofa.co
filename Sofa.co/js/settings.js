96f38a3bd1ccf8355724ff26f53fe40c67aadaf3
Maxim Krouk <maximkrouk@gmail.com>
2019-01-07 20:30:28 +0300

    Settings now can be saved

--- Sofa.co/js/settings.js
+++ Sofa.co/js/settings.js
@@ -1,7 +1,10 @@
 'use strict';
 
+
 // Модальное окно
 var isInitialized = false;
+var DefaultAccentColor = "#FA7268";
+var DefaultBgColor = "#5F4B8B";
 var CurrentSettings = {
 	"BgOption": "",
 	"ModeOption": "",
@@ -57,10 +60,12 @@ $('.dropdown-menu').click(function (e) {
 $('#btnSaveSettings').click(function () {
 	console.log("Ну тут тип сохранение настроек");
 	console.log(CurrentSettings);
+	save_options();
 });
 
 function SetEventHandlers(Options) {
 	for (var option of Options) {
+		SetCurrentOptionState(option);
 		option.addEventListener('click', function (e) {
 			var data = TrimByCamelcase(e.target.id);
 			console.log(data);
@@ -77,6 +82,14 @@ function SetEventHandlers(Options) {
 	}
 };
 
+function SetCurrentOptionState(Option) {
+	var parsedOption = TrimByCamelcase(Option.id);
+	if (CurrentSettings[parsedOption[1] + parsedOption[2]] == parsedOption[0])
+	{
+		Option.classList.add("active");
+	}
+}
+
 function TrimByCamelcase(str) {
 	var result = [];
 	var temp = "";
@@ -91,19 +104,38 @@ function TrimByCamelcase(str) {
 	return result;
 }
 
-// function save_options() {
-//     var color = document.getElementById('color').value;
-//     var likesColor = document.getElementById('like').checked;
-//     chrome.storage.sync.set({
-//       favoriteColor: color,
-//       likesColor: likesColor
-//     }, function() {
-//       // Update status to let user know options were saved.
-//       var status = document.getElementById('status');
-//       status.textContent = 'Options saved.';
-//       console.log("Someshit done")
-//       setTimeout(function() {
-//         status.textContent = '';
-//       }, 750);
-//     });
-//   }
+function save_options() {
+	chrome.storage.local.set({
+		settingsList: CurrentSettings
+	}, function () {
+		// Update status to let user know options were saved.
+		//var status = document.getElementById('status');
+		//status.textContent = 'Options saved.';
+		//setTimeout(function () {
+		//status.textContent = '';
+		//}, 750);
+		console.log("Someshit done")
+	});
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+	chrome.storage.local.get({
+		"settingsList": {
+			"BgOption": "default",
+			"ModeOption": "timer",
+			"AnalOption": "year",
+			"CountOption": "down",
+			"UnitsOption": "sofas",
+			"LangOption": "english"
+		}
+	}, function (items) {
+		CurrentSettings = items.settingsList;
+		console.log(items.settingsList)
+	});
+}
+
+
+document.addEventListener('DOMContentLoaded', restore_options);
+
