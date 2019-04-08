273b6129cef8aa29779633c8764196d5de47953f
Maxim Krouk <maximkrouk@gmail.com>
2019-04-08 22:57:58 +0300

    Some code refactored

--- Sofa.co/js/DateTimeAnalyzer.js
+++ Sofa.co/js/DateTimeAnalyzer.js
@@ -1,27 +1,27 @@
-'use strict';
+'use strict'
 
 window.onload = function () {
 	updateScreenData()
 }
 
 function updateScreenData() {
-	var now = new Date();
+	var now = new Date()
 	var temp = "Year"
 	switch (temp) {
 	case "Year":
-		document.getElementById('Percentage').innerHTML = Math.round(MsConverter.ToYearPercentage(GetYearMS(), isLeap(now.getFullYear()))) + "%";
-		document.getElementById('Counter').innerHTML = Math.ceil(MsConverter.ToDays(GetMsToNextYear()));
+		document.getElementById('Percentage').innerHTML = Math.round(MsConverter.ToYearPercentage(GetYearMS(), isLeap(now.getFullYear()))) + "%"
+		document.getElementById('Counter').innerHTML = Math.ceil(MsConverter.ToDays(GetMsToNextYear()))
 		document.getElementById('Progress').style.width = MsConverter.ToYearPercentage(GetYearMS(), isLeap(now.getFullYear())) + "%"
 	}
-	window.setTimeout(updateScreenData(), GetMsToTomorrow());
+	window.setTimeout(updateScreenData(), GetMsToTomorrow())
 }
 
 function GetDayMS() {
 	// get {currentTime obj.} and {today obj. (no h/s/ms)}
-	var now = new Date();
-	var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+	var now = new Date()
+	var today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
 
-	return now - today;
+	return now - today
 }
 
 function GetMsToTomorrow() {
@@ -34,51 +34,79 @@ function GetMsToTomorrow() {
 
 function GetYearMS() {
 	// get {currentTime obj.} and {thisYear obj. (no h/s/ms)}
-	var now = new Date();
-	var thisYear = new Date(now.getFullYear(), 0, 0);
+	var now = new Date()
+	var thisYear = new Date(now.getFullYear(), 0, 0)
 
-	return now - thisYear;
+	return now - thisYear
 }
 
 function GetMsToNextYear() {
 	// get {currentTime obj.} and {nextYear obj. (no h/s/ms)}
-	var now = new Date();
-	var nextYear = new Date(now.getFullYear() + 1, 0, 0);
+	var now = new Date()
+	var nextYear = new Date(now.getFullYear() + 1, 0, 0)
 
-	return nextYear - now;
+	return nextYear - now
 }
 
 function isLeap(year) {
-	return Boolean(new Date(year, 1, 29).getDate() === 29);
+	return Boolean(new Date(year, 1, 29).getDate() === 29)
 }
 
 class MsConverter {
 
 	static ToSeconds(ms) {
-		return (ms / 1000);
-	}
+		return (ms / 1000)
+   }
 
 	static ToMinutes(ms) {
-		return (this.ToSeconds(ms) / 60);
+		return (this.ToSeconds(ms) / 60)
 	}
 
 	static ToHours(ms) {
-		return (this.ToMinutes(ms) / 60);
+		return (this.ToMinutes(ms) / 60)
 	}
 
 	static ToDays(ms) {
-		return (this.ToHours(ms) / 24);
+		return (this.ToHours(ms) / 24)
 	}
 
 	static ToYearPercentage(ms, isLeap) {
 		if (isLeap) {
-			return (this.ToDays(ms) / 3.65);
+			return (this.ToDays(ms) / 3.65)
 		} else {
-			return (this.ToDays(ms) / 3.66);
+			return (this.ToDays(ms) / 3.66)
 		}
 	}
 
 	static ToDayPercentage(ms) {
 		return ((1 - this.ToDays(ms)) * 100);
-	}
+   }
+   
+   static fromSeconds(s) {
+      return s * 1000
+   }   
+   
+   static fromMinutes(m) {
+      return this.fromSeconds(m * 60)
+   }
+
+   static fromHours(h) {
+      return this.fromMinutes(h * 60)
+   }
+   
+   static fromDays(d) {
+      return this.fromHours(d * 24)
+   }
+
+   static fromYearPercentage(percentage, isLeap) {
+      if (isLeap) {
+         return this.fromDays(percentage * 3.65)
+		} else {
+         return this.fromDays(percentage * 3.66)
+		}
+   }
+
+   static fromDayPercentage(percentage) {
+      return this.fromDays(percentage / 100)
+   }
 }
